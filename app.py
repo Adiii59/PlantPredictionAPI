63c1351aab129c9125631321116e6f96d07f1600
Adiii59 <65038718+Adiii59@users.noreply.github.com>
2023-10-20 15:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = None
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder = "templates")
     app.app_context().push()
     return app
 
 app = create_app()
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 from api import *
